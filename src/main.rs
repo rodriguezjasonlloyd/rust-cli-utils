26e1dd7514368b4dc240d7b3a5bf52b9013742b2
rodriguezjasonlloyd <rodriguezjasonlloyd@gmail.com>
2024-05-27 01:38:13 +0800

    added matching

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,20 @@ use std::env::args;
 fn main() {
     let mut iter = args().skip(1).peekable();
 
-    while let Some(arg) = iter.next() {
-        print!("{arg}");
+    if let Some(command) = iter.next() {
+        match command.to_lowercase().as_str() {
+            "echo" => {
+                while let Some(arg) = iter.next() {
+                    print!("{arg}");
 
-        if iter.peek().is_some() {
-            print!(" ");
+                    if iter.peek().is_some() {
+                        print!(" ");
+                    }
+                }
+
+                println!();
+            }
+            _ => println!("Invalid Command Line Utility"),
         }
     }
 }
